702e57b8031a78d39bb6cc8db82b3249e230a7d6
jgmarquesm <joaogabrielmarques@discente.ufg.br>
2022-05-01 01:39:41 -0300

    Adicionando sistema de salvamento de senhas

--- Password-Generator/Password Generator.py
+++ Password-Generator/Password Generator.py
@@ -1,5 +1,9 @@
+from datetime import datetime
 import string
 import random
+import os
+
+symbols = "!$%&()*+,-./:;<=>?@[\]^_`{|}~"
 
 def init():
     print()
@@ -21,7 +25,7 @@ def password1(size):
     return pw
 
 def password2(size):
-    all_char = string.ascii_letters + string.digits + string.punctuation
+    all_char = string.ascii_letters + string.digits + symbols
     pw = ""
     for char in range(size):
         rand_char = random.choice(all_char)
@@ -29,12 +33,12 @@ def password2(size):
     return pw
 
 def password3(size):
-    all_char = string.ascii_letters + string.digits + string.punctuation
+    all_char = string.ascii_letters + string.digits + symbols
     pw = ""
     pw += random.choice(string.ascii_uppercase)
     pw += random.choice(string.ascii_lowercase)
     pw += random.choice(string.digits)
-    pw += random.choice(string.punctuation)
+    pw += random.choice(symbols)
     for char in range(size-4):
         rand_char = random.choice(all_char)
         pw += rand_char
@@ -43,6 +47,7 @@ def password3(size):
 def result(pw):
     print()
     print("The generated password is:", pw)
+    print("Your password has been saved in a txt file.")
     print("_______________________________________________________________________________")
 
 def print_(x):
@@ -70,67 +75,75 @@ while condition:
         i = 0
         case = int(case)
         if case == 1:
+            kind = "Numeric"
             print()
-            m = input("How many passwords you want to? ")
-            if m == "stop":
+            size_in = input("How many characters do you need in this password? ")
+            size_in = size_in.lower()
+            if size_in == "stop":
                 quit()
-            n = int(m)
-            while i < n:
-                print()
-                size_in = input("How many characters do you need in this password? ")
-                size_in = size_in.lower()
-                if size_in == "stop":
-                    quit()
-                print()
-                pw_size = int(size_in)
-                password = password1(pw_size)
-                result(password)
-                i += 1
+            print()
+            pw_size = int(size_in)
+            password = password1(pw_size)
+            result(password)
         elif case == 2:
+            kind = "Alphanumeric without reqs"
             print()
-            m = input("How many passwords you want to? ")
-            if m == "stop":
+            size_in = input("How many characters do you need in this password? ")
+            size_in = size_in.lower()
+            if size_in == "stop":
                 quit()
-            n = int(m)
-            while i < n:
-                print()
-                size_in = input("How many characters do you need in this password? ")
-                size_in = size_in.lower()
-                if size_in == "stop":
-                    quit()
-                print()
-                pw_size = int(size_in)
-                password = password2(pw_size)
-                result(password)
-                i += 1
+            print()
+            pw_size = int(size_in)
+            password = password2(pw_size)
+            result(password)
         elif case == 3:
+            kind = "Alphanumeric with reqs"
             print()
-            m = input("How many passwords you want to? ")
-            if m == "stop":
+            size_in = input("How many characters do you need in this password? ")
+            size_in = size_in.lower()
+            if size_in == "stop":
                 quit()
-            n = int(m)
-            while i < n:
+            pw_size = int(size_in)
+            if pw_size < 4:
+                print_("You need to enter at least 4 in this case.")
                 print()
-                size_in = input("How many characters do you need in this password? ")
+                pw_size = int(input("How many characters do you need in this password? "))
                 size_in = size_in.lower()
-                if size_in == "stop":
+                if size_in == "stop":     
                     quit()
-                print()
-                pw_size = int(size_in)
-                if pw_size < 4:
-                    print_("You need to enter at least 4 in this case.")
-                else:
-                    password = password3(pw_size)
-                    result(password)                
-                    i += 1
+                password = password3(pw_size)
+                result(password)
+            else:   
+                password = password3(pw_size)
+                result(password)                
         else:
-            print_("Please, enter a valid case.")            
+            print_("Please, enter a valid case.")
+            break
+        print()
+        now = datetime.now()
+        date  = now.strftime("%d-%m-%Y %H:%M:%S")
+        print("If  you enter a invalid entry here, it will be assumed you want to stop.")
+        customize = input("[Y/N] Do you want to customize the file name? ")
+        customize = customize.lower()
+
+        if customize == "y":
+            passw = input("Enter the name you want to save it: ")
+            passw += ".txt"
+        elif customize == "n":
+            passw = kind + " Password " + date + ".txt"
+        else:
+            quit()
+        pw_file = open(passw,'w')
+        pw_file.write("Your Password is: "+password)
+        pw_file.close()
+        print("_______________________________________________________________________________")
         print()
         print("If  you enter a invalid entry here, it will be assumed you want to stop.")
         cont = input("[Y/N] Do you want to execute one more time? ")
-        cont = cont.lower()
+        cont = cont.lower() 
         if cont == "y":
             condition = True
+            os.system("clear")
         else:
             condition = False
             print_("Thank you for using this application!")            
